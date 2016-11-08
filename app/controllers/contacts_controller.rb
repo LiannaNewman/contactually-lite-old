be4070bbe60fb8f3c0c8045a21918ebc5367fdbd
Lianna Newman <liannanewman@gmail.com>
2016-11-08 13:14:28 -0500

    Added routes and pages for contacts and users

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,22 +1,44 @@
 class ContactsController < ApplicationController
   def index
+    @contacts = Contact.find_by(user_id)
+    @contacts.all
   end
 
   def show
   end
 
   def new
+    @contact = Contact.new
   end
 
   def create
+    @contact = Contact.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email_address: params[:email_address],
+      phone_number: params[:phone_number],
+      company_name: params[:company_name]
+    )
+    flash[:success] = 'New Contact Added Successfully!'
+    redirect_to '/contacts'
   end
 
   def edit
+    @contact = Contact.find_by(id: params[:id])
   end
 
   def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.update(
+      name: params[:name],
+      email: params[:email]
+    )
   end
 
   def destroy
+    @contact = Contact.find_by(id: params[:id])
+    @contact.destroy
+    flash[:success] = "#{@contact.name} has been removed successfully"
+    redirect_to '/contacts'
   end
 end
